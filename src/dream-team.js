eb56fc1a44c3ebc2716582ef8f8d74cf83828d22
gentaliana <gentaliana@gmail.com>
2020-10-11 18:07:17 +0400

    feat: complite cats and dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members instanceof Array) {
+    let result = '';
+    const arr = members;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'string') {
+        result = arr[i].trim()[0] + result;
+      }
+    }
+    return result.toUpperCase().split('').sort().join('');
+  } else {
+    return false;
+  }
 };
