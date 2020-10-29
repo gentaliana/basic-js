a40d2aeb154c8889ffdf7a6f3014a752a3c48298
gentaliana <gentaliana@gmail.com>
2020-10-29 21:08:56 +0400

    feat: complited task  transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    const newArr = [];
+    const copyArr = [...arr];
+    for (let i = 0; i < copyArr.length; i++) {
+      switch (copyArr[i]) {
+        case '--discard-next':
+          if (copyArr[i + 1] !== undefined) {
+            i = i + 1;
+            copyArr[i] = undefined;
+          }
+          break;
+        case '--discard-prev':
+          if (copyArr[i - 1] !== undefined) {
+            newArr.pop();
+          }
+          break;
+        case '--double-next':
+          if (copyArr[i + 1] !== undefined) {
+            newArr.push(copyArr[i + 1]);
+          }
+          break;
+        case '--double-prev':
+          if (copyArr[i - 1] !== undefined) {
+            newArr.push(copyArr[i - 1]);
+          }
+          break;
+        default:
+          newArr.push(copyArr[i]);
+      }
+    }
+    return newArr;
+  } else {
+    throw new Error();
+  }
 };
