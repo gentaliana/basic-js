99714a32c60e2e40421a0dddd93a9b122c8d5da7
gentaliana <gentaliana@gmail.com>
2020-10-30 16:58:23 +0400

    feat: complited task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,31 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 const chainMaker = {
+  result: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || !this.result[position - 1]) {
+      this.result = [];
+      throw new Error('error');
+    };
+    this.result.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const newRes = this.result.join('~~');
+    this.result = [];
+    return newRes;
+  },
 };
 
 module.exports = chainMaker;
